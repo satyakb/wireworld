3e4310ae789943f3b28f01eddc4670dba8e933e9
Satya Bodduluri <skbodduluri@gmail.com>
2016-07-08 00:03:23 -0700

    made it nicer

--- main.js
+++ main.js
@@ -9,19 +9,19 @@ const CONDUCTOR = 3;
 
 /********************* Default Evaluation Functions **************************/
 function empty($tile, numNeighbors) {
-  if ($tile.data('color') === EMPTY) return EMPTY;
+  if ($tile.data('state') === EMPTY) return EMPTY;
 }
 
 function head($tile, numNeighbors) {
-  if ($tile.data('color') === HEAD) return TAIL;
+  if ($tile.data('state') === HEAD) return TAIL;
 }
 
 function tail($tile, numNeighbors) {
-  if ($tile.data('color') === TAIL) return CONDUCTOR;
+  if ($tile.data('state') === TAIL) return CONDUCTOR;
 }
 
 function conductor($tile, numNeighbors) {
-  if ($tile.data('color') === CONDUCTOR) {
+  if ($tile.data('state') === CONDUCTOR) {
     if (numNeighbors === 1 || numNeighbors === 2) return HEAD;
   }
 }
@@ -34,7 +34,7 @@ var fns = [
 ];
 /*****************************************************************************/
 
-
+// Main
 $(function() {
   var X = 5;
   var Y = 10;
@@ -52,9 +52,7 @@ $(function() {
   $pause.click(function() {
     isPaused = true;
   });
-  $tick.click(function() {
-    loop();
-  });
+  $tick.click(loop);
 
   $app.append($play);
   $app.append($pause);
@@ -67,6 +65,9 @@ $(function() {
 
 });
 
+/**
+ * Function to be called in a loop
+ */
 function loop() {
   // Capture new state of grid
   var tmpGrid = [];
@@ -77,11 +78,13 @@ function loop() {
 
     for (var j = 0; j < grid[0].length; j++) {
       var $tile = grid[i][j];
-      var numNeighbors = checkNeighbors(i, j);
+      var numNeighbors = calcNumNeighbors(i, j);
       var changed = false;
 
       for (var k = 0; k < fns.length; k++) {
         var newColor = fns[k]($tile, numNeighbors);
+
+        // If function has a result, use it
         if (newColor) {
           row.push(newColor);
           changed = true;
@@ -89,7 +92,8 @@ function loop() {
         }
       }
 
-      if (!changed) row.push($tile.data('color'));
+      // Use old state if none of the supplied functions have any effect
+      if (!changed) row.push($tile.data('state'));
     }
 
     tmpGrid.push(row);
@@ -98,16 +102,17 @@ function loop() {
   // Update grid state
   for (var i = 0; i < grid.length; i++) {
     for (var j = 0; j < grid[0].length; j++) {
-      updateTile(grid[i][j], tmpGrid[i][j]);
+      updateTile(i, j, tmpGrid[i][j]);
     }
   }
 }
 
-function updateTile($tile, color) {
-  $tile.data('color', color);
-  $tile.css('background-color', colorMap[color]);
-}
-
+/**
+ * Creates a grid
+ * @param  {int} x    x dimension of grid (number of rows)
+ * @param  {int} y    y dimension of grid (number of columns)
+ * @return {jQuery}   grid jquery object
+ */
 function createGrid(x, y) {
   var count = 0;
   var $grid = $("<div>", {class: 'grid'});
@@ -115,7 +120,7 @@ function createGrid(x, y) {
     var row = [];
     var $row = $("<div>", {class: 'tile-row', id: 'row-' + i});
     for (var j = 0; j < y; j++) {
-      $tile = createTileWithId(count++);
+      $tile = createTile(i, j, count++);
       $row.append($tile);
       row.push($tile);
     }
@@ -125,39 +130,63 @@ function createGrid(x, y) {
   return $grid;
 }
 
-function createTileWithId(id) {
-  var $tile = $("<div>", {class: 'tile', id: 'tile-' + id});
-  $tile.data('num', id);
-  $tile.data('color', EMPTY);
+/**
+ * Creates a single tile with id
+ * @param  {int} x      row index of the tile
+ * @param  {int} y      column index of the tile
+ * @return {jQuery}     the created tile
+ */
+function createTile(x, y, num) {
+  var $tile = $("<div>", {class: 'tile'});
+  $tile.data('num', num);
+  $tile.data('state', EMPTY);
   $tile.css('background-color', colorMap[0]);
 
   $tile.click(function(){
     var num = $tile.data('num');
-    var color = $tile.data('color');
+    var color = $tile.data('state');
 
     var newColor = (color + 1) % colorMap.length;
-    updateTile($tile, newColor);
+    updateTile(x, y, newColor);
   });
 
   return $tile;
 }
 
-function checkNeighbors(i, j) {
+/**
+ * Changes state of single $tile to state
+ * @param  {int} x          row index of the tile
+ * @param  {int} y          column index of the tile
+ * @param  {int} newState   new state
+ */
+function updateTile(x, y, newState) {
+  var $tile = grid[x][y];
+  $tile.data('state', newState);
+  $tile.css('background-color', colorMap[newState]);
+}
+
+/**
+ * Calculates the number of HEAD neighbors of a tile
+ * @param  {int} x row index of the tile
+ * @param  {int} y column index of the tile
+ * @return {int}   number of HEAD neighbors
+ */
+function calcNumNeighbors(x, y) {
   var num = 0;
 
-  var top = Math.max(i - 1, 0);
-  var bottom = Math.min(i + 1, grid.length - 1);
-  var left = Math.max(j - 1, 0);
-  var right = Math.min(j + 1, grid[0].length - 1);
+  var top = Math.max(x - 1, 0);
+  var bottom = Math.min(x + 1, grid.length - 1);
+  var left = Math.max(y - 1, 0);
+  var right = Math.min(y + 1, grid[0].length - 1);
 
-  if (grid[top][j].data('color') === 1) num++;
-  if (grid[top][right].data('color') === 1) num++;
-  if (grid[i][right].data('color') === 1) num++;
-  if (grid[bottom][right].data('color') === 1) num++;
-  if (grid[bottom][j].data('color') === 1) num++;
-  if (grid[bottom][left].data('color') === 1) num++;
-  if (grid[i][left].data('color') === 1) num++;
-  if (grid[top][left].data('color') === 1) num++;
+  if (grid[top][y].data('state') === HEAD) num++;
+  if (grid[top][right].data('state') === HEAD) num++;
+  if (grid[x][right].data('state') === HEAD) num++;
+  if (grid[bottom][right].data('state') === HEAD) num++;
+  if (grid[bottom][y].data('state') === HEAD) num++;
+  if (grid[bottom][left].data('state') === HEAD) num++;
+  if (grid[x][left].data('state') === HEAD) num++;
+  if (grid[top][left].data('state') === HEAD) num++;
 
   return num;
 }
